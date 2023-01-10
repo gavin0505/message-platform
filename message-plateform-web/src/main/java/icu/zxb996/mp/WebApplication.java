acbeeae374b63c47a51cc3ec8f52a9cc8f4ff40a
Gavin Zhang <zxb_worky@163.com>
2023-01-10 17:03:48 +0800

    init

---
+++ message-plateform-web/src/main/java/icu/zxb996/mp/WebApplication.java
@@ -0,0 +1,16 @@
+package icu.zxb996.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gavin Zhang
+ * @date 2023/1/10 16:58
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
