efbd4b50711c25d2853757c46e19aed8756e3591
Gavin Zhang <zxb_worky@163.com>
2023-01-12 16:49:06 +0800

    增加-发送内容的模型类

---
+++ message-plateform-common/src/main/java/icu/zxb996/mp/common/dto/ContentModel.java
@@ -0,0 +1,13 @@
+package icu.zxb996.mp.common.dto;
+
+import java.io.Serializable;
+
+/**
+ * 发送内容的模型
+ * (不同的渠道会有不同的消息体)
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/12 16:34
+ */
+public class ContentModel implements Serializable {
+}
