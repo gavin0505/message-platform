a656463fbb0f980be9effdf917626a8e47590ef2
Gavin Zhang <zxb_worky@163.com>
2023-01-12 17:02:49 +0800

    更新-发送消息任务模型

--- message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/domain/SendTaskModel.java
+++ message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/domain/SendTaskModel.java
@@ -1,9 +1,14 @@
 package icu.zxb996.mp.service.api.impl.domain;
 
+import icu.zxb996.mp.common.domain.TaskInfo;
+import icu.zxb996.mp.service.api.domain.MessageParam;
+import icu.zxb996.mp.support.domain.MessageTemplate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * 发送消息信息任务模型
@@ -17,4 +22,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SendTaskModel {
+
+
+    /**
+     * 消息模板Id
+     */
+    private Long messageTemplateId;
+
+    /**
+     * 请求参数
+     */
+    private List<MessageParam> messageParamList;
+
+    /**
+     * 发送任务的信息
+     */
+    private List<TaskInfo> taskInfo;
+
+    /**
+     * 撤回任务的信息
+     */
+    private MessageTemplate messageTemplate;
 }
