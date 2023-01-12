7fd4aad1920457c30783b326d640623178eaea42
Gavin Zhang <zxb_worky@163.com>
2023-01-12 14:07:12 +0800

    创建-发送消息信息任务模型

---
+++ message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/domain/SendTaskModel.java
@@ -0,0 +1,20 @@
+package icu.zxb996.mp.service.api.impl.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 发送消息信息任务模型
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/12 13:59
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SendTaskModel {
+}
