c759f5c94bc23269a24216ab217c8a51ebbc6a9c
Gavin Zhang <zxb_worky@163.com>
2023-01-12 17:21:21 +0800

    增加-责任链上下文类

---
+++ message-plateform-support/src/main/java/icu/zxb996/mp/support/pipeline/ProcessContext.java
@@ -0,0 +1,43 @@
+package icu.zxb996.mp.support.pipeline;
+
+import icu.zxb996.mp.common.vo.ResultVO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 责任链上下文
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/12 17:15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class ProcessContext<T extends ProcessModel> {
+
+    /**
+     * 标识责任链的code
+     */
+    private String code;
+
+    /**
+     * 存储责任链上下文数据的模型
+     */
+    private T processModel;
+
+    /**
+     * 责任链中断的标识
+     */
+    private Boolean needBreak;
+
+    /**
+     * 流程处理的结果
+     */
+    ResultVO<String> response;
+
+}
