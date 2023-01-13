e0b9079077d744b6107821a130aec65ff77de184
Gavin Zhang <zxb_worky@163.com>
2023-01-13 18:01:07 +0800

    增加前置参数校验类

---
+++ message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/action/PreParamCheckAction.java
@@ -0,0 +1,60 @@
+package icu.zxb996.mp.service.api.impl.action;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import icu.zxb996.mp.common.enums.RespStatusEnum;
+import icu.zxb996.mp.common.vo.ResultVO;
+import icu.zxb996.mp.service.api.domain.MessageParam;
+import icu.zxb996.mp.service.api.impl.domain.SendTaskModel;
+import icu.zxb996.mp.support.pipeline.BusinessProcess;
+import icu.zxb996.mp.support.pipeline.ProcessContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 前置参数校验
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/13 15:23
+ */
+@Slf4j
+@Service("preParamCheckAction")
+public class PreParamCheckAction implements BusinessProcess<SendTaskModel> {
+
+    /**
+     * 最大的人数
+     */
+    private static final Integer BATCH_RECEIVER_SIZE = 100;
+
+    @Override
+    public void process(ProcessContext<SendTaskModel> context) {
+        SendTaskModel sendTaskModel = context.getProcessModel();
+
+        Long messageTemplateId = sendTaskModel.getMessageTemplateId();
+        List<MessageParam> messageParamList = sendTaskModel.getMessageParamList();
+
+        // 1.若没有传入 消息模板Id 或者 messageParam
+        if (messageTemplateId == null || CollUtil.isEmpty(messageParamList)) {
+            context.setNeedBreak(true).setResponse(ResultVO.error(RespStatusEnum.CLIENT_BAD_PARAMETERS.getCode(), RespStatusEnum.CLIENT_BAD_PARAMETERS.getMsg()));
+            return;
+        }
+
+        // 2.过滤 receiver=null 的messageParam
+        List<MessageParam> resultMessageParamList = messageParamList.stream()
+                .filter(messageParam -> !StrUtil.isBlank(messageParam.getReceiver()))
+                .collect(Collectors.toList());
+        if (CollUtil.isEmpty(resultMessageParamList)) {
+            context.setNeedBreak(true).setResponse(ResultVO.error(RespStatusEnum.CLIENT_BAD_PARAMETERS.getCode(), RespStatusEnum.CLIENT_BAD_PARAMETERS.getMsg()));
+            return;
+        }
+        sendTaskModel.setMessageParamList(resultMessageParamList);
+
+        // 3.过滤receiver大于100的请求
+        if (messageParamList.stream().anyMatch(messageParam -> messageParam.getReceiver().split(StrUtil.COMMA).length > BATCH_RECEIVER_SIZE)) {
+            context.setNeedBreak(true).setResponse(ResultVO.error(RespStatusEnum.TOO_MANY_RECEIVER.getCode(), RespStatusEnum.TOO_MANY_RECEIVER.getMsg()));
+        }
+    }
+}
