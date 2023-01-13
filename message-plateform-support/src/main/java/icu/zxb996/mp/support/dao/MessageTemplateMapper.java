e227d55b89c4c2b25bf0f64037fecd8d0e565595
Gavin Zhang <zxb_worky@163.com>
2023-01-13 15:50:40 +0800

    增加-MessageTemplateMapper映射

---
+++ message-plateform-support/src/main/java/icu/zxb996/mp/support/dao/MessageTemplateMapper.java
@@ -0,0 +1,13 @@
+package icu.zxb996.mp.support.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import icu.zxb996.mp.support.domain.MessageTemplate;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Gavin Zhang
+ * @date 2023/1/13 14:50
+ */
+@Mapper
+public interface MessageTemplateMapper extends BaseMapper<MessageTemplate> {
+}
