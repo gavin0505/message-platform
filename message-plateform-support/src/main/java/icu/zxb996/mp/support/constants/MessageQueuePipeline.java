81234f636fc28e28a2166e5f7bee6604b0689ead
Gavin Zhang <zxb_worky@163.com>
2023-01-14 17:11:45 +0800

    增加-消息队列常量

---
+++ message-plateform-support/src/main/java/icu/zxb996/mp/support/constants/MessageQueuePipeline.java
@@ -0,0 +1,12 @@
+package icu.zxb996.mp.support.constants;
+
+/**
+ * 消息队列常量
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/14 16:55
+ */
+public interface MessageQueuePipeline {
+
+    String KAFKA = "kafka";
+}
