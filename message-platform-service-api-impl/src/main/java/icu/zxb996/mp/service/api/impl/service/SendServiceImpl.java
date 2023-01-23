604eca8902efcbb6dd1c842856e4064e49cfab20
Gavin Zhang <zxb_worky@163.com>
2023-01-23 13:32:13 +0800

    完成-消息发送功能实现类

--- message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/service/SendServiceImpl.java
+++ message-platform-service-api-impl/src/main/java/icu/zxb996/mp/service/api/impl/service/SendServiceImpl.java
@@ -2,8 +2,14 @@ package icu.zxb996.mp.service.api.impl.service;
 
 import icu.zxb996.mp.common.vo.ResultVO;
 import icu.zxb996.mp.service.api.domain.SendRequest;
+import icu.zxb996.mp.service.api.impl.domain.SendTaskModel;
 import icu.zxb996.mp.service.api.service.SendService;
+import icu.zxb996.mp.support.pipeline.ProcessContext;
+import icu.zxb996.mp.support.pipeline.ProcessController;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
 
 /**
  * @author Gavin Zhang
@@ -12,10 +18,25 @@ import org.springframework.stereotype.Service;
 @Service("sendServiceImpl")
 public class SendServiceImpl implements SendService {
 
+    @Resource(name = "processController")
+    private ProcessController processController;
 
     @Override
     public ResultVO<String> send(SendRequest sendRequest) {
 
-        return null;
+        // 组装发送消息信息任务模型
+        SendTaskModel sendTaskModel = SendTaskModel.builder()
+                .messageTemplateId(sendRequest.getMessageTemplateId())
+                .messageParamList(Collections.singletonList(sendRequest.getMessageParam()))
+                .build();
+
+        ProcessContext context = ProcessContext.builder()
+                .code(sendRequest.getCode())
+                .processModel(sendTaskModel)
+                .needBreak(false)
+                .response(ResultVO.success("")).build();
+
+        ProcessContext process = processController.process(context);
+        return ResultVO.success(process.getResponse().getCode(), process.getResponse().getMessage());
     }
 }
