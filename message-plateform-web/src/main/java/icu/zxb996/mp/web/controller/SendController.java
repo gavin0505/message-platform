bb15349d90087bec9e121b73a767020ba1d43936
Gavin Zhang <zxb_worky@163.com>
2023-01-23 13:32:43 +0800

    完善-消息发送接口

--- message-plateform-web/src/main/java/icu/zxb996/mp/web/controller/SendController.java
+++ message-plateform-web/src/main/java/icu/zxb996/mp/web/controller/SendController.java
@@ -1,10 +1,16 @@
 package icu.zxb996.mp.web.controller;
 
 import icu.zxb996.mp.common.vo.ResultVO;
+import icu.zxb996.mp.service.api.domain.SendRequest;
+import icu.zxb996.mp.service.api.service.SendService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author Gavin Zhang
@@ -16,8 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"发送消息接口"})
 public class SendController {
 
+    @Resource(name = "sendServiceImpl")
+    private SendService sendService;
+
     @PostMapping("/send")
-    public ResultVO<String> send() {
-        return null;
+    @ApiOperation("发送消息")
+    public ResultVO<String> send(@RequestBody SendRequest sendRequest) {
+        return sendService.send(sendRequest);
     }
 }
