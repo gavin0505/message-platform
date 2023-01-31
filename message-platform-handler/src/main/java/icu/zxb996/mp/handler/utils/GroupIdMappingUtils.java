164aef0255b0a7c07e31e0089adb75b974e74aaf
Gavin Zhang <zxb_worky@163.com>
2023-01-31 16:13:25 +0800

    增加GroupId映射器

---
+++ message-platform-handler/src/main/java/icu/zxb996/mp/handler/utils/GroupIdMappingUtils.java
@@ -0,0 +1,45 @@
+package icu.zxb996.mp.handler.utils;
+
+import icu.zxb996.mp.common.domain.TaskInfo;
+import icu.zxb996.mp.common.enums.ChannelType;
+import icu.zxb996.mp.common.enums.MessageType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * GroupId映射器
+ * groupId 标识着每一个消费者组
+ *
+ * @author Gavin Zhang
+ * @date 2023/1/31 15:48
+ */
+public class GroupIdMappingUtils {
+
+    /**
+     * 获取所有的groupIds
+     * (不同的渠道不同的消息类型拥有自己的groupId)
+     */
+    public static List<String> getAllGroupIds() {
+        List<String> groupIds = new ArrayList<>();
+        for (ChannelType channelType : ChannelType.values()) {
+            for (MessageType messageType : MessageType.values()) {
+                groupIds.add(channelType.getCodeEn() + "." + messageType.getCodeEn());
+            }
+        }
+        return groupIds;
+    }
+
+
+    /**
+     * 根据TaskInfo获取当前消息的groupId
+     *
+     * @param taskInfo 消息体
+     * @return 当前消息的groupId
+     */
+    public static String getGroupIdByTaskInfo(TaskInfo taskInfo) {
+        String channelCodeEn = ChannelType.getEnumByCode(taskInfo.getSendChannel()).getCodeEn();
+        String msgCodeEn = MessageType.getEnumByCode(taskInfo.getMsgType()).getCodeEn();
+        return channelCodeEn + "." + msgCodeEn;
+    }
+}
