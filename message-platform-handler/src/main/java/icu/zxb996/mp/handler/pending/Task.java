13a428ca32b47852c98c49799cfb268eb3600b39
Gavin Zhang <zxb_worky@163.com>
2023-02-01 16:31:29 +0800

    增加Task发送消息方法

--- message-platform-handler/src/main/java/icu/zxb996/mp/handler/pending/Task.java
+++ message-platform-handler/src/main/java/icu/zxb996/mp/handler/pending/Task.java
@@ -1,11 +1,16 @@
 package icu.zxb996.mp.handler.pending;
 
+import cn.hutool.core.collection.CollUtil;
+import icu.zxb996.mp.common.domain.TaskInfo;
+import icu.zxb996.mp.handler.handler.HandlerHolder;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 消息发送任务
@@ -20,6 +25,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Task implements Runnable {
+
+    @Resource
+    private HandlerHolder handlerHolder;
+    private TaskInfo taskInfo;
+
     @Override
     public void run() {
 
@@ -27,5 +37,8 @@ public class Task implements Runnable {
         // 2. 屏蔽消息
         // 3. 平台通用去重
         // 4. 发送消息
+        if (CollUtil.isNotEmpty(taskInfo.getReceiver())) {
+            handlerHolder.route(taskInfo.getSendChannel()).doHandler(taskInfo);
+        }
     }
 }
