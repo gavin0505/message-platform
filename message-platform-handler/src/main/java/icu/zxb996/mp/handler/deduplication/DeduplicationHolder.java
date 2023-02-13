e3a96a7af1adb98e76548abcc072f300c2e8a2a1
Gavin Zhang <zxb_worky@163.com>
2023-02-13 13:14:27 +0800

    增加-去重服务处理器的相关注释

--- message-platform-handler/src/main/java/icu/zxb996/mp/handler/deduplication/DeduplicationHolder.java
+++ message-platform-handler/src/main/java/icu/zxb996/mp/handler/deduplication/DeduplicationHolder.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 去重功能保持器
+ * 去重服务处理器
  *
  * @author Gavin Zhang
  * @date 2023/2/7 16:15
@@ -16,14 +16,26 @@ import java.util.Map;
 @Service
 public class DeduplicationHolder {
 
-
+    /**
+     * 去重参数映射器
+     */
     private final Map<Integer, Builder> builderHolder = new HashMap<>(4);
+
+    /**
+     * 去重功能映射器
+     */
     private final Map<Integer, DeduplicationService> serviceHolder = new HashMap<>(4);
 
+    /**
+     * 找到对应去重参数构造器
+     */
     public Builder selectBuilder(Integer key) {
         return builderHolder.get(key);
     }
 
+    /**
+     * 找到对应去重服务构造器
+     */
     public DeduplicationService selectService(Integer key) {
         return serviceHolder.get(key);
     }
