58468ceed6a65bcdb63f7bded17b27db8875e66b
Gavin Zhang <zxb_worky@163.com>
2023-02-14 16:16:39 +0800

    增加-限流控制

--- message-platform-handler/src/main/java/icu/zxb996/mp/handler/handler/BaseHandler.java
+++ message-platform-handler/src/main/java/icu/zxb996/mp/handler/handler/BaseHandler.java
@@ -3,10 +3,13 @@ package icu.zxb996.mp.handler.handler;
 import icu.zxb996.mp.common.domain.AnchorInfo;
 import icu.zxb996.mp.common.domain.TaskInfo;
 import icu.zxb996.mp.common.enums.AnchorState;
+import icu.zxb996.mp.handler.flowcontrol.FlowControlFactory;
+import icu.zxb996.mp.handler.flowcontrol.FlowControlParam;
 import icu.zxb996.mp.support.utils.LogUtils;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.Objects;
 
 /**
  * 发送各个渠道的handler
@@ -28,6 +31,18 @@ public abstract class BaseHandler implements Handler {
     protected Integer channelCode;
 
     /**
+     * 限流控制工厂
+     */
+    @Resource(name = "flowControlFactory")
+    private FlowControlFactory flowControlFactory;
+
+    /**
+     * 限流相关的参数
+     * 子类初始化的时候指定
+     */
+    protected FlowControlParam flowControlParam;
+
+    /**
      * 初始化渠道与Handler的映射关系
      */
     @PostConstruct
@@ -35,8 +50,23 @@ public abstract class BaseHandler implements Handler {
         handlerHolder.putHandler(channelCode, this);
     }
 
+    /**
+     * 流量控制
+     *
+     * @param taskInfo 消息体
+     */
+    public void flowControl(TaskInfo taskInfo) {
+        // 只有子类指定了限流参数，才需要限流
+        if (Objects.nonNull(flowControlParam)) {
+            flowControlFactory.flowControl(taskInfo, flowControlParam);
+        }
+    }
+
     @Override
     public void doHandler(TaskInfo taskInfo) {
+
+        // 限流控制
+        flowControl(taskInfo);
         // 通过handler接口发送信息
         if (handler(taskInfo)) {
             logUtils.print(AnchorInfo.builder().state(AnchorState.SEND_SUCCESS.getCode()).businessId(taskInfo.getBusinessId()).ids(taskInfo.getReceiver()).build());
