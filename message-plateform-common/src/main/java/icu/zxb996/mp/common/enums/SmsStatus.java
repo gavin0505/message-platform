5cdca1b5c0bfeb816f0d79dc752cfae716bb780f
Gavin Zhang <zxb_worky@163.com>
2023-03-02 22:24:19 +0800

    增加-短信状态信息枚举

---
+++ message-plateform-common/src/main/java/icu/zxb996/mp/common/enums/SmsStatus.java
@@ -0,0 +1,54 @@
+package icu.zxb996.mp.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * 短信状态信息
+ *
+ * @author Gavin Zhang
+ * @date 2023/3/2 19:48
+ */
+
+@Getter
+@ToString
+@AllArgsConstructor
+public enum SmsStatus {
+
+    /**
+     * 调用渠道接口发送成功
+     */
+    SEND_SUCCESS(10, "调用渠道接口发送成功"),
+    /**
+     * 用户收到短信(收到渠道短信回执，状态成功)
+     */
+    RECEIVE_SUCCESS(20, "用户收到短信(收到渠道短信回执，状态成功)"),
+    /**
+     * 用户收不到短信(收到渠道短信回执，状态失败)
+     */
+    RECEIVE_FAIL(30, "用户收不到短信(收到渠道短信回执，状态失败)"),
+    /**
+     * 调用渠道接口发送失败
+     */
+    SEND_FAIL(40, "调用渠道接口发送失败");
+
+    private final Integer code;
+    private final String description;
+
+
+    /**
+     * 根据状态获取描述信息
+     *
+     * @param code 状态码
+     * @return 描述信息
+     */
+    public static String getDescriptionByStatus(Integer code) {
+        for (SmsStatus value : SmsStatus.values()) {
+            if (value.getCode().equals(code)) {
+                return value.getDescription();
+            }
+        }
+        return "";
+    }
+}
