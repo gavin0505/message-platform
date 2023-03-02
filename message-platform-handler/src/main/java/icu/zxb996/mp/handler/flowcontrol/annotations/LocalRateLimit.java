3ecb9ef19271c7127007c123cfa373d7238281f4
Gavin Zhang <zxb_worky@163.com>
2023-03-02 15:15:28 +0800

    修改-补充限流注解

--- message-platform-handler/src/main/java/icu/zxb996/mp/handler/flowcontrol/annotations/LocalRateLimit.java
+++ message-platform-handler/src/main/java/icu/zxb996/mp/handler/flowcontrol/annotations/LocalRateLimit.java
@@ -1,6 +1,9 @@
 package icu.zxb996.mp.handler.flowcontrol.annotations;
 
 import icu.zxb996.mp.handler.enums.RateLimitStrategy;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.*;
 
 /**
  * 单机限流注解
@@ -8,6 +11,10 @@ import icu.zxb996.mp.handler.enums.RateLimitStrategy;
  * @author Gavin Zhang
  * @date 2023/2/14 15:49
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Service
 public @interface LocalRateLimit {
 
     RateLimitStrategy rateLimitStrategy() default RateLimitStrategy.REQUEST_RATE_LIMIT;
